61520c7edab0e271ad322e48195d58d9efc0dd02
82orez <82orez@naver.com>
2023-12-04 04:09:12 +0900

    삼항 연산자 적용 3

--- passport.js
+++ passport.js
@@ -37,7 +37,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK_URL || 'https://localhost:4000/auth/google/callback',
+      callbackURL:
+        process.env.NODE_ENV === 'production' ? 'https://api.infothings.net/auth/google/callback' : 'https://localhost:4000/auth/google/callback',
       scope: ['email', 'profile'],
     },
     async (accessToken, refreshToken, profile, done) => {
